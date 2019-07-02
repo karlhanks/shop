6410687bc351a9afb9e7ffe3a2cb684609fe9730
karlhanks <942899256@qq.com>
2019-07-02 18:20:15 +0800

    商品页拆分

---
+++ demo/src/components/NavBread.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="NavBread">
+    <nav class="nav-breadcrumb">
+      <!-- <a href="/">首页</a>
+      <span>商品列表</span> -->
+      <slot></slot>
+    </nav>
+  </div>
+</template>
+ 
+<script>
+export default {};
+</script>
+ 
+<style scoped >
+@import url("../assets/css/header.css");
+</style>
