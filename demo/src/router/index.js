14931c2e89d38f4f2a28d13a44b0db0405c84d39
karlhanks <942899256@qq.com>
2019-07-02 19:19:17 +0800

    全部静态页面完成

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Goodlist from '@/views/Goodlist'
+import Cart from '@/views/Cart'
+import Address from '@/views/Address'
+import OrderConfirm from '@/views/OrderConfirm'
+import OrderSuccess from '@/views/OrderSuccess'
 
 Vue.use(Router)
 
@@ -10,7 +14,28 @@ export default new Router({
       path: '/',
       name: 'Goodlist',
       component: Goodlist
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/address',
+      name: 'Address',
+      component: Address
+    },
+    {
+      path: '/orderConfirm',
+      name: 'OrderConfirm',
+      component: OrderConfirm
+    },
+    {
+      path: '/orderSuccess',
+      name: 'OrderSuccess',
+      component: OrderSuccess
     }
+
   ]
 })
     
